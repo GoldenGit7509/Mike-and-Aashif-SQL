18893e5f864faa3855e08f515b3fa2104fb979c8
GoldenGit7509 <aashif.abdullah@student.englisches-institut.eu>
2025-06-03 19:30:08 +0200

    Add files via upload
    3.6.25   19:30
    Links, rechts bewegung + Code ist mehr geordnet
    Ich mache jetz den Sprung

--- one.py
+++ one.py
@@ -1,16 +1,57 @@
 import pygame
 import random
 
-# Initialisierung
+
 pygame.init()
-WIDTH, HEIGHT = 800, 400
+HEIGHT = 800
+WIDTH = 1000
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Jump and Run")
 
+
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
+
+
+spieler = pygame.Rect(100, 300, 40, 40)
+spieler_speed = 5
+
+
+
+
+
+clock = pygame.time.Clock()
+fps = 60
+
+
 running = True
 while running:
+    screen.fill(WHITE)
 
- for event in pygame.event.get():
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+   
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        spieler.x -= spieler_speed
+    if keys[pygame.K_RIGHT]:
+        spieler.x += spieler_speed
+    
+
+
+
+    
+    pygame.draw.rect(screen, BLUE, spieler)
+    
+
+    
+    pygame.display.flip()
+    clock.tick(fps)
+
+pygame.quit()
+
+
 
